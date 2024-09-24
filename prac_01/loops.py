276c237a714aefc09b43288f976be9197b0ccc31
juliarngold <julia.arngold@my.jcu.edu.au>
2024-09-24 18:13:24 +0800

    loops

---
+++ prac_01/loops.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 - Practical
+3. Loops to display odd number
+"""
+for i in range(1, 21, 2):
+    print(i, end=' ')
+print()
+
+# a. count in 10s from 0 to 100:
+for i in range(0, 101, 10):
+    print(i, end=' ')
+print()
+
+# b. count down from 20 to 1:
+for i in range(20, 0, -1):
+    print(i, end=' ')
+print()
+
+# c. print n stars
+number = int(input("Enter a number: "))
+for i in range(number):
+    print("*", end=' ')
+
