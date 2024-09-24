de4e4bc23b207472d3f55bbb4c7508aa44913a12
juliarngold <julia.arngold@my.jcu.edu.au>
2024-09-24 20:45:10 +0800

    finished code

--- prac_01/shop_calculator.py
+++ prac_01/shop_calculator.py
@@ -4,7 +4,14 @@ CP1404/CP5632 - Practical
 """
 total = 0
 number_of_items = int(input("Number of items: "))
+while number_of_items <0:
+    print("Invalid number of items!")
+    number_of_items = int(input("Number of items: "))
 for i in range(number_of_items):
     price = float(input("Price of item: "))
     total += price
 print(f"Total price for {number_of_items} items is ${total:.2f}")
+number_of_items = int(input("Number of items: "))
+
+
+
