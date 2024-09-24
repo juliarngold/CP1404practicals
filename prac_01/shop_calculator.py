15f84adf93c608bb55fb8d9130e6acb9109076b0
juliarngold <julia.arngold@my.jcu.edu.au>
2024-09-24 18:38:01 +0800

    code without looping

---
+++ prac_01/shop_calculator.py
@@ -0,0 +1,10 @@
+"""
+CP1404/CP5632 - Practical
+4. Shop Calculator
+"""
+total = 0
+number_of_items = int(input("Number of items: "))
+for i in range(number_of_items):
+    price = float(input("Price of item: "))
+    total += price
+print(f"Total price for {number_of_items} items is ${total:.2f}")
