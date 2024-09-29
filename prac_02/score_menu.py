0cf440c5436b245a11d1a69013dfa26108bc5978
juliarngold <julia.arngold@my.jcu.edu.au>
2024-09-29 22:05:02 +0800

    copy standard pattern

---
+++ prac_02/score_menu.py
@@ -0,0 +1,21 @@
+"""
+CP1404/CP5632 - Practical
+Menu
+
+
+display menu
+get choice
+while choice != <quit option>
+    if choice == <first option>
+        <do first task>
+    else if choice == <second option>
+        <do second task>
+    ...
+    else if choice == <n-th option>
+        <do n-th task>
+    else
+        display invalid input error message
+    display menu
+    get choice
+<do final thing, if needed>
+"""
