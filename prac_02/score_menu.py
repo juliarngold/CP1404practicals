9bd7586547596ea592623ca2e34ffad0206e039c
juliarngold <julia.arngold@my.jcu.edu.au>
2024-09-29 22:16:06 +0800

    main function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -19,3 +19,32 @@ while choice != <quit option>
     get choice
 <do final thing, if needed>
 """
+MINIMAL_SCORE_THRESHOLD = 0
+MAXIMUM_SCORE_THRESHOLD = 100
+EXCELLENT_SCORE_THRESHOLD = 90
+BAD_SCORE_THRESHOLD = 50
+VALID_CHOICES = "gpsq"
+VALID_HIGH_THRESHOLD = 100
+VALID_LOW_THRESHOLD = 0
+DEFAULT_SCORE = 0
+MENU = """(G)et a valid score
+(P)rint result
+(S)how stars
+(Q)uit"""
+
+def main():
+    score = DEFAULT_SCORE
+    print(MENU)
+    choice = get_valid_choice()
+    while choice != "q":
+        if choice == "g":
+            score = get_valid_score()
+        elif choice == "p":
+            print(determine_score(score))
+        elif choice == "s":
+            print_asterisks(score)
+        else:
+            print("Invalid choice")
+print(MENU)
+        choice = get_valid_choice()
+    print("Farewell")
