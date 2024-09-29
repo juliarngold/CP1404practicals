28ccec81be0afab1ab7c9d50470df0a20b4f5603
juliarngold <julia.arngold@my.jcu.edu.au>
2024-09-29 20:52:30 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,11 +2,23 @@
 CP1404/CP5632 - Practical
 Refactoring- Password Check with Functions
 """
-
 MINIMUM_LENGTH= 4
-password= input("Enter a password: ")
-while len(password) < MINIMUM_LENGTH:
-    print("Invalid password")
+def main():
+    password = get_password()
+    display_password(password)
+
+
+def display_password(password):
+    for i in range(len(password)):
+        print("*", end="")
+
+
+def get_password():
     password = input("Enter a password: ")
-for i in range(len(password)):
-    print("*", end="")
+    while len(password) < MINIMUM_LENGTH:
+        print("Invalid password")
+        password = input("Enter a password: ")
+    return password
+
+
+main()
