cbce4836f15da4b33320013563de44ff804d4cf9
juliarngold <julia.arngold@my.jcu.edu.au>
2024-09-29 21:29:25 +0800

    corrected errors

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -13,9 +13,9 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            convert_celsius_into_fahrenheit()
+            print(convert_celsius_into_fahrenheit(float(input("Celsius: "))))
         elif choice == "F":
-            convert_fahrenheit_into_celsius()
+            print(convert_fahrenheit_into_celsius(float(input("Fahrenheit: "))))
         else:
             print("Invalid option")
         print(MENU)
@@ -23,16 +23,14 @@ def main():
     print("Thank you.")
 
 
-def convert_fahrenheit_into_celsius():
-    fahrenheit = float(input("Fahrenheit: "))
+def convert_fahrenheit_into_celsius(fahrenheit):
     celsius = 5 / 9 * (fahrenheit - 32)
-    print(f"Result: {celsius:.2f} C")
+    return f"{celsius:.2f}"
 
 
-def convert_celsius_into_fahrenheit():
-    celsius = float(input("Celsius: "))
+def convert_celsius_into_fahrenheit(celsius):
     fahrenheit = celsius * 9.0 / 5 + 32
-    print(f"Result: {fahrenheit:.2f} F")
+    return f"{fahrenheit:.2f}"
 
 
 main()
