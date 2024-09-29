8dbff87eef9171e901fdef07738a57b67b4de242
juliarngold <julia.arngold@my.jcu.edu.au>
2024-09-29 21:53:11 +0800

    refactored code to determine score statues, corrected

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,23 +3,33 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-
 # TODO: Fix this!
 
+import random
+
+VALID_MINIMAL_SCORE_THRESHOLD = 0
+VALID_MAXIMUM_SCORE_THRESHOLD = 100
+EXCELLENT_SCORE_THRESHOLD = 90
+BAD_SCORE_THRESHOLD = 50
+
+
 def main():
+    """determine and display score status"""
     score = float(input("Enter score: "))
-    while 0 <= score <= 100:
-        message = determine_result(score)
-        print(message)
-        score = float(input("Enter score: "))
-    print("Invalid score")
+    print(determine_score(score))
+    print(determine_score(random.randint(0, 100)))
 
 
-def determine_result(score):
-    if score >= 90:
-        message = "Excellent"
-    elif score >= 50:
-        message = "Passable"
+def determine_score(score):
+    """determine score status"""
+    if score < VALID_MINIMAL_SCORE_THRESHOLD or score > VALID_MAXIMUM_SCORE_THRESHOLD:
+        return "Invalid score"
+    elif score >= EXCELLENT_SCORE_THRESHOLD:
+        return "Excellent"
+    elif score >= BAD_SCORE_THRESHOLD:
+        return "Passable"
     else:
-        message = "Bad"
-    return message
+        return "Bad"
+
+
+main()
