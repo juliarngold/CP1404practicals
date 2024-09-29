c19d85d193fb6cdbe2c7036299577a4fcfdb9fe5
juliarngold <julia.arngold@my.jcu.edu.au>
2024-09-29 22:33:14 +0800

    Display a menu that allows the user to get a valid score, print a result, show asterisks, or quit.

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -19,6 +19,7 @@ while choice != <quit option>
     get choice
 <do final thing, if needed>
 """
+
 MINIMAL_SCORE_THRESHOLD = 0
 MAXIMUM_SCORE_THRESHOLD = 100
 EXCELLENT_SCORE_THRESHOLD = 90
@@ -32,7 +33,9 @@ MENU = """(G)et a valid score
 (S)how stars
 (Q)uit"""
 
+
 def main():
+    """Display a menu that allows the user to get a valid score, print a result, show asterisks, or quit."""
     score = DEFAULT_SCORE
     print(MENU)
     choice = get_valid_choice()
@@ -45,6 +48,42 @@ def main():
             print_asterisks(score)
         else:
             print("Invalid choice")
-print(MENU)
+        print(MENU)
         choice = get_valid_choice()
     print("Farewell")
+
+
+def get_valid_choice():
+    """get valid input for menu choice"""
+    choice = input(">>> ").lower()
+    while choice not in VALID_CHOICES:
+        print("Invalid choice")
+        choice = input(">>> ")
+    return choice
+
+
+def get_valid_score():
+    """get valid score"""
+    score = int(input("Enter score: "))
+    while score < VALID_LOW_THRESHOLD or score > VALID_HIGH_THRESHOLD:
+        print("Invalid score")
+        score = int(input("Enter score: "))
+    return score
+
+
+def print_asterisks(length):
+    """display asterisks"""
+    print("*" * length)
+
+
+def determine_score(score):
+    """determine status of score"""
+    if score >= EXCELLENT_SCORE_THRESHOLD:
+        return "Excellent"
+    elif score >= BAD_SCORE_THRESHOLD:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
