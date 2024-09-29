cec265c39c45ec2252d48b2145ef07c25b53197f
juliarngold <julia.arngold@my.jcu.edu.au>
2024-09-29 21:42:56 +0800

    refactor code to dtermine score status

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,16 +3,23 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+
 # TODO: Fix this!
 
-score = float(input("Enter score: "))
-while 0 <= score <= 100:
+def main():
+    score = float(input("Enter score: "))
+    while 0 <= score <= 100:
+        message = determine_result(score)
+        print(message)
+        score = float(input("Enter score: "))
+    print("Invalid score")
+
+
+def determine_result(score):
     if score >= 90:
-        message= "Excellent"
+        message = "Excellent"
     elif score >= 50:
         message = "Passable"
     else:
         message = "Bad"
-    print(message)
-    score = float(input("Enter score: "))
-print("Invalid score")
+    return message
