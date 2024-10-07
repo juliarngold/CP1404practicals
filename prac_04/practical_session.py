02aaf8381de94a1bed2c7bea5e0551b96ae82f3b
juliarngold <julia.arngold@my.jcu.edu.au>
2024-10-07 16:30:46 +0800

    remove name from names

---
+++ prac_04/practical_session.py
@@ -0,0 +1,11 @@
+names = ["Ada", "Alan", "Bill", "John"]
+print(",".join(names))
+name_to_remove = input("What do you want to remove? ")
+while len(names) != 0:
+    try:
+        names.remove(name_to_remove)
+    except ValueError:
+        print("Please try again.")
+    print(names)
+    name_to_remove = input("What do you want to remove? ")
+
