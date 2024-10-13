2da20f22a9bed4e7913489504da9915a6476020f
juliarngold <julia.arngold@my.jcu.edu.au>
2024-10-13 20:38:49 +0800

    is username in usernames

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -12,3 +12,14 @@ print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 
+
+""" Ask the user for their username. If the username is in the above list of authorised users, 
+print "Access granted", otherwise print "Access denied" """
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter your name: ")
+if username in usernames:
+    message = "Access granted"
+else:
+    message = "Access denied"
+print(message)
