09e355f9dabf144f4d8c2a44042e328eefc33958
juliarngold <julia.arngold@my.jcu.edu.au>
2024-11-03 23:53:25 +0800

    encapsulate the functionality that make the class more helpful

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,5 +1,13 @@
+"""Estimated time: 15 min, actual time: 11 min"""
+
 from prac_06.programming_language import ProgrammingLanguage
 
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+languages = [python, ruby, visual_basic]
+print("The dynamically typed languages are:")
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)
