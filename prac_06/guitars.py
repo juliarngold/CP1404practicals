080a1a09d6247972c36a4cfdd8e056169ad0ee68
juliarngold <julia.arngold@my.jcu.edu.au>
2024-11-04 00:00:33 +0800

    use guitar class to write a code

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,19 +1,55 @@
-class Guitar:
-    def __init__(self, name="", year=0,cost=0):
-        self.name = name
-        self.year = year
-        self.cost = cost
+"""
+Estimated time: 20 min
+Actual time: 25 min
+"""
 
-    def __str__(self):
-        return f"{self.name} ({self.year}) : ${self.cost}"
+from prac_06.guitar import Guitar
 
-    def get_age(self):
-        """get age"""
-        return 2024 - self.year
 
-    def is_vintage(self):
-        """is vintage if its 50 or older"""
-        if self.get_age() >= 50:
-            return True
-        else:
-            return False
+def main():
+    print("My guitars!")
+    guitars = []
+    name = input("Name: ")
+    while name != "":
+        year = get_valid_year()
+        cost = get_valid_cost()
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} ({year}) : ${cost} added.")
+        name = input("Name: ")
+
+    print("These are my guitars:")
+    print_guitars(guitars)
+
+
+def print_guitars(guitars):
+    """Display collection of guitars"""
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = "(vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
+
+
+def get_valid_cost():
+    """Get valid guitar's cost"""
+    while True:
+        try:
+            cost = float(input("Cost: $"))
+            break
+        except ValueError:
+            print("Invalid cost")
+            pass
+    return cost
+
+
+def get_valid_year():
+    """Get valid guitar's year"""
+    while True:
+        try:
+            year = float(input("Year: "))
+            break
+        except ValueError:
+            print("Invalid year")
+            pass
+    return year
+
+
+main()
