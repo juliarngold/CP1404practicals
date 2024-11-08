2266f2f9b3156a4f4e012d0d6fc901ae11ac473d
juliarngold <julia.arngold@my.jcu.edu.au>
2024-11-08 16:04:50 +0800

    open file, create class objects and store into a list

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -6,17 +6,31 @@ def main():
     in_file = open('guitars.csv', 'r')
     for line in in_file:
         parts = line.strip().split(',')
-        guitar = parts[0]  # store only the guitar name
-        guitars.append(guitar)
-    in_file.close()
-    print(guitars)
 
-    # guitar =
-    # guitars.append(guitar)
-    #
-    # # Loop through and display all languages (using their str method)
-    # for guitar in guitars:
-    #     print(guitar)
+        guitar = Guitar(parts[0], parts[1], parts[2])  # use parts of the lines in csv-file to create class objects
+        guitars.append(guitar)  # append guitar object to list of guitars
+        # print(guitar)
+        print(guitars)
+        print(type(guitar))
+        # print(parts)
+    in_file.close()
+    # print(guitar_names)
+
+#     sort_guitar_years()
+#
+#
+#
+# def sort_guitar_years(guitars):
+#     for guitar in guitars:
+
+
+
+# guitar =
+# guitars.append(guitar)
+#
+# # Loop through and display all languages (using their str method)
+# for guitar in guitars:
+#     print(guitar)
 
 
 main()
