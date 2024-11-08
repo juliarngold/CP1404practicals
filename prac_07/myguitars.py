90d90ce6cb1083dff1fde2c6516134eca687b1fb
juliarngold <julia.arngold@my.jcu.edu.au>
2024-11-08 19:41:43 +0800

    add new guitar to guitars list with check for error checking

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -9,23 +9,49 @@ def main():
         guitar = Guitar(parts[0], parts[1], parts[2])  # use parts of the lines in csv-file to create class objects
         guitars.append(guitar)  # append guitar object to list of guitars
     in_file.close()
+    display_guitars_sorted_by_year(guitars)
+    add_new_guitar_to_list(guitars)
 
+
+def add_new_guitar_to_list(guitars):
+    print("Let's add a new guitar")
+    name = input("Name: ")
+    while name != "":
+        year = get_valid_year()
+        cost = get_valid_cost()
+        guitars.append(Guitar(name, year, cost))  # add new guitar to guitar list
+        print(f"{name} ({year}) : ${cost} added.")
+        name = input("Name: ")
+
+
+def display_guitars_sorted_by_year(guitars):
     guitars.sort()  # sort guitars by year
     for guitar in guitars:
         print(guitar)
-    add_new_guitar(guitars)
-    # for guitar in guitars:
-    #     print(guitar)
 
 
-def add_new_guitar(guitars):
-    print("Lets add a new guitar")
-    new_guitar_name = input('Enter a guitar name: ')
-    new_guitar_year = input('Enter a guitar year: ')
-    new_guitar_cost = input('Enter a guitar cost: ')
-    guitar = Guitar(new_guitar_name, new_guitar_year, new_guitar_cost)
-    guitars.append(guitar)
+def get_valid_cost():
+    """Get valid guitar's cost"""
+    while True:
+        try:
+            cost = float(input("Cost: $"))
+            break
+        except ValueError:
+            print("Invalid cost")
+            pass
+    return cost
 
+
+def get_valid_year():
+    """Get valid guitar's year"""
+    while True:
+        try:
+            year = float(input("Year: "))
+            break
+        except ValueError:
+            print("Invalid year")
+            pass
+    return year
 
 
 
