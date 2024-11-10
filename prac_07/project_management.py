9a99bdf5c459738ad81e886680fbf55457799d27
juliarngold <julia.arngold@my.jcu.edu.au>
2024-11-10 23:36:53 +0800

    declare constants

---
+++ prac_07/project_management.py
@@ -0,0 +1,20 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+Project Management Program.
+Estimated time: 60 min (8:50)
+Actual: 140 min
+"""
+from prac_07.project import Project
+import datetime
+
+VALID_CHOICES = "LSDFAUQ"
+MENU = "- (L)oad projects\n" \
+       "- (S)ave projects\n" \
+       "- (D)isplay projects\n" \
+       "- (F)ilter projects by date\n" \
+       "- (A)dd new project\n" \
+       "- (U)pdate project\n" \
+       "- (Q)uit"
+
+
+
