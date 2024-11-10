7c293dc84384f7e731a38868fff954bd429a0414
juliarngold <julia.arngold@my.jcu.edu.au>
2024-11-10 23:38:49 +0800

    add and update projects

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -19,6 +19,92 @@ MENU = "- (L)oad projects\n" \
 
 
 
+def update_project(projects):
+    """Update status of existing project"""
+    for i, project in enumerate(projects, 0):
+        print(f"{i} " + str(project))
+    while True:
+        try:
+            project_choice = int(input("Project choice: "))
+            print(projects[project_choice])
+            break
+        except ValueError:
+            print("Invalid project number")
+        except IndexError:
+            print("Invalid project number")
+    while True:
+        try:
+            percentage = input("New Percentage: ")
+            if percentage == "":
+                break
+            percentage = int(percentage)
+            if 0 <= percentage <= 100:
+                projects[project_choice].completion_percentage = percentage
+                break
+            else:
+                print("Number must be >= 0 and <= 100")
+        except ValueError:
+            print("Invalid input")
+    while True:
+        priority = input("New Priority: ")
+        if priority == "":
+            break
+        try:
+            priority = int(priority)
+            if 0 < priority:
+                projects[project_choice].priority = priority
+                projects.sort(key=lambda x: x.priority)
+                break
+            else:
+                print("Number must be > 0")
+        except ValueError:
+            print("Invalid input")
+
+
+def add_project(projects):
+    """Add new project to the list"""
+    print("Let's add a new project")
+    name = input("Name: ")
+    while True:
+        try:
+            start_date = input("Start date (dd/mm/yyyy): ")
+            start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
+            break
+        except ValueError:
+            print("Invalid date")
+
+    while True:
+        try:
+            priority = int(input("Priority: "))
+            if 0 < priority:
+                break
+            else:
+                print("Number must be > 0")
+        except ValueError:
+            print("Invalid input")
+
+    while True:
+        try:
+            cost_estimate = int(input("Cost estimate: $"))
+            if 0 <= cost_estimate:
+                break
+            else:
+                print("Number must be >= 0")
+        except ValueError:
+            print("Invalid input")
+    while True:
+        try:
+            percentage = int(input("Percent complete: "))
+            if 0 <= percentage <= 100:
+                break
+            else:
+                print("Number must be >= 0 and <= 100")
+        except ValueError:
+            print("Invalid input")
+    projects.append(Project(name, start_date.strftime("%d/%m/%Y"), priority, cost_estimate, percentage))
+    projects.sort(key=lambda x: x.priority)
+
+
 def display_projects(projects):
     """Print list of loaded projects"""
     print("Incomplete projects: ")
