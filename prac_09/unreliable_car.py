466185f79237581873a4d775e244a31b473d6a1b
juliarngold <julia.arngold@my.jcu.edu.au>
2024-11-24 17:37:13 +0800

    defines subclass, UnreliableCar

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -4,10 +4,13 @@ import random
 
 class UnreliableCar(Car):
     def __init__(self, name, fuel, reliability=0.0):
+        """Initialise a UnreliableCar instance."""
         super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
+        """Attempt to drive a given distance. The car drives the distance if it passes a reliability check and has enough fuel.
+    If fuel is insufficient, it drives until fuel runs out. If reliability check fails, the car does not move."""
         if self.reliability > random.randint(0, 100):
             if distance > self.fuel:
                 distance = self.fuel
